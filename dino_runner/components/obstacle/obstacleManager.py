8447b850896bd64922f9a42e7448a3eef73bb97d
Aldo Ochoa <ochoaaldoochoa@gmail.com>
2022-12-08 02:17:10 -0400

    grupo1-mod2 el dinosaru - clase 3

--- dino_runner/components/obstacle/obstacleManager.py
+++ dino_runner/components/obstacle/obstacleManager.py
@@ -15,11 +15,30 @@ class ObstacleManager():
             if game.player.dino_rect.colliderect(obstacle):
                 pygame.time.delay(500)
                 game.playing = False
-                break
+                #break
+                game.death_count = game.death_count + 1
+                
+                pygame.time.delay(100)
+                self.obstacles
+
+                #game.player_heart_manager.reduce_heart()    ----> error del trainer 
+                if game.player_heart_manager.heart_count > 0:
+                    game.player.show_text =  False
+                    
+                else:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count = game.death_count + 1
+                    break
+                  
+
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacle(self, self1):
+        self.obstacles = []
 
 
 
